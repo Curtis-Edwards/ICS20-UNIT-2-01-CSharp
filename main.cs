8de4493d7ce086e2eaef8eaddba87de9e828c6c7
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-03-10 01:44:11 +0000

    removed whitespace

--- main.cs
+++ main.cs
@@ -2,11 +2,11 @@
 
 using System;
 
-class Program 
+class Program
 {
-    public static void Main (string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine ("Hello, World!");
+        Console.WriteLine("Hello, World!");
         Console.WriteLine("\nDone.");
     }
 }
